8d666fff0c1b58cab1878fccff573ec42dc2a150
kidmortal <kidmortal@gmail.com>
2024-02-15 21:27:56 -0300

    fix

--- index.js
+++ index.js
@@ -6,20 +6,15 @@ const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.get('/', async (req, res) => {
-  const imageUrl = req.query.url;
-  const token = req.query.token;
-  const url = `${imageUrl}&token=${token}.png`
+  const url3 = req.url.split("url=")[1]
+  console.log(url3)
   const url2 = "https://firebasestorage.googleapis.com/v0/b/smartimob-dev-test.appspot.com/o/empresas%2FweOGq249Mw1CRI7npBVM%2FQP1eYu5bbqQgINSG9118%2Fhtkswcz?alt=media&token=22012311-0198-41da-95f6-7d60e1a51ca7.png";
-
-  console.log(req.query)
-  console.log(encodeURI(url))
-  console.log(url2)
-  if (!url) {
+  if (!url3) {
     return res.status(400).send('Missing "url" parameter.');
   }
 
   try {
-    const response = await axios.get(url2, { responseType: 'arraybuffer' });
+    const response = await axios.get(url3, { responseType: 'arraybuffer' });
     const resizedImageBuffer = await sharp(response.data)
       .resize({ height: 300 })
       .toBuffer();
